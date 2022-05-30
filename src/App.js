f7d5d9737a78e6601e641eb66bee2e6c57730ae3
Alvin <alvin.tol@hotmail.com>
2022-05-30 10:30:11 -0600

    adds hardcoded data to be used for api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import axios from 'axios'
 
+const hardcodeAPIKEY = '';
+const hardcodeCity = 'Calgary';
+const hardcodeCountry = 'Canada';
+
 function App() {
+
+  const URL = `https://api.openweathermap.org/data/2.5/weather?q=${hardcodeCity},${hardcodeCountry}&appid=${hardcodeAPIKEY}`;
+
   return (
     <div>
       test
