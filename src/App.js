ca707e1df0bc9d495292a16ad14fa75bbefbadea
Alvin <alvin.tol@hotmail.com>
2022-05-30 10:38:37 -0600

    adds env file to mask apikey

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import axios from 'axios'
 
-const hardcodeAPIKEY = '';
 const hardcodeCity = 'Calgary';
 const hardcodeCountry = 'Canada';
 
 function App() {
 
-  const URL = `https://api.openweathermap.org/data/2.5/weather?q=${hardcodeCity},${hardcodeCountry}&appid=${hardcodeAPIKEY}`;
+  const URL = `https://api.openweathermap.org/data/2.5/weather?q=${hardcodeCity},${hardcodeCountry}&appid=${process.env.REACT_APP_API_KEY}`;
 
   return (
     <div>
