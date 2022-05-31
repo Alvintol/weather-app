521cedd50af7eba5f0ffe0f02d5440e9f773b72e
Alvin <alvin.tol@hotmail.com>
2022-05-30 20:30:53 -0600

    adds moment.js

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import Weekdays from './Weekdays';
 const App = () => {
 
   const { state, buttonToggle } = useAppData();
-  const icon = {value: `https://openweathermap.org/img/wn/${state?.data?.current?.weather[0].icon}@x2.png`}
-
 
   return (
     <div className='app'>
@@ -23,21 +21,13 @@ const App = () => {
       {state.button === 'CLICKED' &&
         <div className='container'>
           <div className='top'>
-            <div className='cityIcon'>
-              <div className='location'>
-                <p>Calgary</p>
-              </div>
-              <div className='weatherIcon'>
-                <img src={icon?.value} alt='weather icon'></img>
-              </div>
-            </div>
             <div className='currentTemp'>
-              <h1> {state?.data.current.temp}ºC</h1>
+              <h1>Your Location: <span className='yourTemp'>{state?.data.current.temp}ºC</span></h1>
             </div>
           </div>
           <Weekdays
-          key='weekday'
-          state={state}
+            key='weekday'
+            state={state}
           />
         </div>
       }
